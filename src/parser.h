bd38b66695b21f83429943e9663358ae960fc58c
Aly Gaber <aly.gaber@mail.mcgill.ca>
2026-05-24 17:44:50 -0400

    started parser

---
+++ src/parser.h
@@ -0,0 +1,38 @@
+#ifndef PARSER_H
+#define PARSER_H
+#include "lexer.h"
+
+typedef enum
+{
+    NODE_NUMBER,
+    NODE_BINOP,
+    NODE_NAME,
+    NODE_IF,
+    NODE_ASSIGN,
+    NODE_PRINT,
+    NODE_FUNCTION_DEF,
+    NODE_FUNCTION_CALL,
+    NODE_RETURN,
+    NODE_BLOCK
+} NodeType;
+
+typedef struct ASTNode
+{
+    NodeType type;
+    union
+    {
+        int num_value;
+        char op;
+        char name[64];
+    } data;
+    struct ASTNode *left;
+    struct ASTNode *right;
+    struct ASTNode *next;
+} ASTNode;
+
+ASTNode *ast_init(NodeType nodetype);
+ASTNode *parse_program(Token *tokens, int *pos);
+ASTNode *parse_statement(Token *tokens, int *pos);
+ASTNode *parse_expression(Token *tokens, int *pos);
+
+#endif
